0cc340a158f7d6ca706f05bf807e4466b1c33bd8
moises <moiseszandonai1234@gmail.com>
2024-03-21 20:28:30 -0300

    Ex 01 finish

---
+++ src/lista02/ex1/Employee.java
@@ -0,0 +1,23 @@
+package lista02.ex1;
+
+public class Employee {
+
+    private double salary;
+
+    public double lastSalary;
+
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+    public double getSalary(){
+        return this.salary;
+    }
+
+    public void applyRaise(double pcRaise){
+        double firstSalary = this.getSalary();
+        double LastSalary = firstSalary * (1+ (pcRaise /100));
+        this.lastSalary = firstSalary;
+        this.setSalary(LastSalary);
+        System.out.println("The employee have applyRaise, end salary = "+this.getSalary());
+    }
+}
---
+++ src/lista02/ex1/Main.java
@@ -0,0 +1,15 @@
+package lista02.ex1;
+
+public class Main {
+
+   public static void main(String[] args){
+       Employee employee = new Employee();
+
+       double salary = 1477.85;
+       double pcRaise = 10;
+       employee.setSalary(salary);
+       employee.applyRaise(pcRaise);
+       System.out.println("A DIFERENÇA É " + (employee.getSalary() - employee.lastSalary));
+   }
+
+}
