3ead55a013a8667f6acce3f47c8becd999464f89
moises <moiseszandonai1234@gmail.com>
2024-03-21 21:41:24 -0300

    dexiando em ingres

--- src/lista02/ex2/Main.java
+++ src/lista02/ex2/Main.java
@@ -2,7 +2,7 @@ package lista02.ex2;
 
 public class Main {
     public static void main(String[] args) {
-        Retangular retangula = new Retangular(20,10,4);
-        retangula.calculeArea();
+        Box box = new Box(20,10,4);
+        box.calculeVolume();
     }
 }
