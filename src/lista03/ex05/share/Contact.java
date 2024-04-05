f8455445bb327209c80f5d4d83dfffac4a07c8a3
moises <moiseszandonai1234@gmail.com>
2024-04-04 22:39:54 -0300

    ex05

---
+++ src/lista03/ex05/share/Contact.java
@@ -0,0 +1,51 @@
+package lista03.ex05.share;
+
+public class Contact {
+
+    private String name;
+
+    private String Address;
+
+    private char Sex;
+
+    private String email;
+
+    public Contact(String name, String address, char sex, String email) {
+        this.name = name;
+        Address = address;
+        Sex = sex;
+        this.email = email;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return Address;
+    }
+
+    public void setAddress(String address) {
+        Address = address;
+    }
+
+    public char getSex() {
+        return Sex;
+    }
+
+    public void setSex(char sex) {
+        Sex = sex;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
---
+++ src/lista03/ex05/share/ContactBook.java
@@ -0,0 +1,32 @@
+package lista03.ex05.share;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ContactBook {
+
+    List<Contact> points = new ArrayList<>();
+
+    public void insertContact(){
+        Contact contact = new Contact();
+        points.add(contact);
+    }
+
+    public void removeContact(String name){
+       for (Contact contact : points) {
+        if(contact.getName() == name){
+            points.remove(contact);
+        }
+       }
+    }
+
+    public void listContacts(){
+        for (Contact contact : points) {
+            System.out.println("Name: " + contact.getName());
+            System.out.println("Sex: " + contact.getSex());
+            System.out.println("Email: " + contact.getEmail());
+            System.out.println("Address: " + contact.getAddress());
+        }
+    }
+
+}
